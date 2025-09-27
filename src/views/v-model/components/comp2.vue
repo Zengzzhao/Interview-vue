c2b24893a6d8b0fcb8631f380f0ad5e4dd8c065b
Zengzzhao <787332942@qq.com>
2025-09-27 11:31:34 +0800

    feat: v-model的demo

---
+++ src/views/v-model/components/comp2.vue
@@ -0,0 +1,26 @@
+<!-- 自定义组件上的底层实现 -->
+<template>
+    <div class="container" @input="handleInput" contenteditable>{{ modelValue }}</div>
+</template>
+
+<script setup lang='ts'>
+const props = defineProps<{
+    modelValue: string
+}>()
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: string): void
+}>()
+
+const handleInput = (e: Event) => {
+    const val = (e.target as HTMLDivElement).innerText
+    emit('update:modelValue', val)
+}
+</script>
+
+<style scoped>
+.container {
+    width: 200px;
+    height: 100px;
+    border: 2px solid #ccc;
+}
+</style>
