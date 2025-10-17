22d55b567bbece3afbd2b7ff1eab973ce6fbcabc
Zengzzhao <787332942@qq.com>
2025-10-17 15:52:47 +0800

    feat: scss的mixin、函数、变量

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,10 +1,24 @@
 <template>
     <div>test</div>
+    <div class="container">aslda</div>
+    <div class="fixBox"></div>
 </template>
 
 <script setup lang='ts'>
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@use '@/style/utils.scss' as *;
+.container{
+    width: 200px;
+    height: 200px;
+    background-color: aqua;
+    @include red_bold_Font
+}
+.fixBox{
+    width: vw(200);
+    height: vh(200);
+    background-color: black;
+    @include center_position(absolute)
+}
 </style>
